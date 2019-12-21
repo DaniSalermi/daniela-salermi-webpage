965385f3fe0d34c2c849df351dd82e600ea0ec34
Daniela Salermi <salermi22@gmail.com>
2019-12-21 19:16:26 -0300

    feat add dynamic recent work data

--- src/app/modules/reusable/work-card/work-card.component.ts
+++ src/app/modules/reusable/work-card/work-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-work-card',
-  templateUrl: './work-card.component.html',
-  styleUrls: ['./work-card.component.scss']
+  selector: "app-work-card",
+  templateUrl: "./work-card.component.html",
+  styleUrls: ["./work-card.component.scss"]
 })
 export class WorkCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() projectName: string;
+  @Input() projectType: string;
+  @Input() imageURL: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
