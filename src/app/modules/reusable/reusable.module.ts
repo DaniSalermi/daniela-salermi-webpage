e16b41f39f4a5ebb3a748a193a7dc16e0367252b
Daniela Salermi <salermi22@gmail.com>
2019-12-21 11:48:33 -0300

    feat add RRSS bar

--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from "./menu/menu.component";
+import { RrssBarComponent } from "./rrss-bar/rrss-bar.component";
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, MenuComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    RrssBarComponent
+  ],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, RrssBarComponent]
 })
 export class ReusableModule {}
---
+++ src/app/modules/reusable/rrss-bar/rrss-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rrss-bar',
+  templateUrl: './rrss-bar.component.html',
+  styleUrls: ['./rrss-bar.component.scss']
+})
+export class RrssBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
