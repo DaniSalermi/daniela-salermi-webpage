988ae2b3a5218ed578f0041df10ddf4e96a68bdb
Daniela Salermi <salermi22@gmail.com>
2019-12-21 09:10:53 -0300

    feat add header

--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, MenuComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, FooterComponent]
 })
